027865950e427e1874728468370ba7ba8c3d077c
McDevin619 <phillipesuon@gmail.com>
2022-04-29 15:09:13 -0700

    fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -28,4 +28,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (s < 0)
 	{
 		free(buf);
+		return (0);
+	}
+	buf[s] = '\0';
 
+	close(fd);
+
+	t = write(STDOUT_FILENO, buf, s);
+	if (t < 0)
+	{
+		free(buf);
+		return (0);
+	}
+
+	free(buf);
+	return (t);
+}
