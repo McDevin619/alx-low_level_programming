5ec663eba8991c3695a9b7032727e7ce16ffd3be
McDevin619 <phillipesuon@gmail.com>
2022-06-13 16:49:58 -0700

    final

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/McDevin619/alx-low_level_programming/blob/735d3c68ac17575a86621fffa4d27535921126fa/0x18-dynamic_libraries/nrandom.so
+wget -P /tmp https://github.com/McDevin619/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
 export LD_PRELOAD=/tmp/nrandom.so
